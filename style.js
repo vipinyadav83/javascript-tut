c67f232b0e08566e223584772e5e994ac52b083b
“vipin <vkyadav8383@gmail.com>
2023-12-05 22:26:22 +0530

    add some string method

--- style.js
+++ style.js
@@ -1,6 +1,16 @@
-let text = "Apple, Banana, Kiwi";
+let text = "   Apple, Banana, Kiwi";
+let a= "text"
+let b= "hai"
 //length
 console.log(text.length);
+var i =1
 //slice()
-const sl = text.slice(-12, -6);
+//const sl = text.slice(-12, -6);
+//const sl  = text.toUpperCase();
+//const sl  = text.toLowerCase();
+//const sl  = text.charAt(8)
+//const sl  = text.substring(2,8)
+//const sl  = text.replace('a','z')
+//const sl  = text.concat(a)
+const sl  = text.trim()
 console.log(sl)
