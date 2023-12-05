519efb5b341942870c4e482dad5a59e4ff02c9c4
“vipin <vkyadav8383@gmail.com>
2023-12-05 21:56:08 +0530

    slice method

--- style.js
+++ style.js
@@ -1,4 +1,6 @@
-var gh = "dhfjk"
- var gh = "woqkdij"
-
-console.log(gh)
+let text = "Apple, Banana, Kiwi";
+//length
+console.log(text.length);
+//slice()
+const sl = text.slice(-12, -6);
+console.log(sl)
